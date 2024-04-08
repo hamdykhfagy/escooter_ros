fd2313175b2e00d11ad70bf03eec7ce8fe0af041
hamdykhfagy <hamdykhfagy@gmail.com>
2024-04-09 00:06:50 +0200

    tutorial scripts

--- srcipts/run_ros_bags.py
+++ srcipts/run_ros_bags.py
@@ -19,32 +19,19 @@ def process_bagfiles(bags_path, result_path):
             counter1 +=1
             bag = rosbag.Bag(bagfile_path)
             image_topic = bag.read_messages("/camera/image_raw")
-            counter = 0
             for b in image_topic:
                 bridge = CvBridge()
                 cv_image = bridge.imgmsg_to_cv2(b.message, desired_encoding="bgr8")
                 # Calculate height of each cropped image
                 cropped_height = IMAGE_HEIGHT // 6
-
-                # List to store cropped images
-                cropped_images = []
-
                 # Crop and append each section
                 for i in range(6):
                     upper = i * cropped_height
                     lower = (i + 1) * cropped_height
                     cropped_img = cv_image[upper:lower, :]
-                    cropped_images.append(cropped_img)
                     resized_image = cv2.resize(cropped_img, (800, 800))
                     rotated_img = cv2.rotate(resized_image, cv2.ROTATE_90_CLOCKWISE)
-                    # cv2.imshow(f'Image_{i}', resized_image)
-                    cv2.imwrite(f'{result_path}/camera_image_raw_{b.message.header.frame_id}_{b.message.header.seq}.jpg', rotated_img)
-                # cv2.destroyAllWindows()
-                # y_min, x_min, h, w = 0, 0 , int(IMAGE_HEIGHT/2), IMAGE_WIDTH
-                # cropped_image = cv_image[y_min:y_min+h, x_min:x_min+w]
-                # cv2.waitKey(0)
-                counter += 1
-            print(f"num of images saved = {counter}")
+                    cv2.imwrite(f'{result_path}/camera_image_raw_seq_{b.message.header.seq}_camId_{i}.jpg', rotated_img)
             bag.close()
 
 if __name__ == "__main__":
